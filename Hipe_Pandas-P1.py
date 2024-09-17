676b69c10e8dc25bb5257cde59dbc5cc31260fc7
venven16 <ravenallysa.hipe.eng@ust.edu.ph>
2024-09-17 19:01:04 +0800

    Update Hipe_Pandas-P1.py

--- Hipe_Pandas-P1.py
+++ Hipe_Pandas-P1.py
@@ -9,6 +9,14 @@ cars = pd.read_csv('cars.csv')
 print("First five rows:")
 cars.head()
 
-# Display the first five rows
+# Display the first 16 rows
+print("First 16 rows:")
+cars.head(16)
+
+# Display the last five rows
 print("Last five rows:")
 cars.tail()
+
+# Display the last 16 rows
+print("Last 16 rows:")
+cars.tail(16)
